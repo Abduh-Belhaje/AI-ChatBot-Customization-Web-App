6dcc4d0da00bd749eae67b07ac509fb746ab4f71
Rida-Lamiini <ridalamiini0@gmail.com>
2024-10-09 14:47:00 +0100

    auth mod

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,29 +1,44 @@
-const passport = require('passport');
-const authService = require("../services/authService")
+const passport = require("passport");
+const authService = require("../services/authService");
 
 // Google Sign In action
 exports.googleSignIn = (req, res, next) => {
-    passport.authenticate('google', { scope: ['profile', 'email'] , state: 'login'})(req, res, next);
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+    state: "login",
+  })(req, res, next);
 };
 
 // Google Sign Up action
 exports.googleSignUp = (req, res, next) => {
-    passport.authenticate('google', { scope: ['profile', 'email'] , state: 'register' })(req, res, next);
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+    state: "register",
+  })(req, res, next);
 };
 
 // Callback after Google authentication
 exports.googleCallback = (req, res, next) => {
-    passport.authenticate('google', { session: false, failureRedirect: '/' }, async (err, user, info) => {
-        if (err || !user) {
-            return res.status(401).json({ error: 'Authentication failed. Please try again.' });
-        }
-        try {
-            const resp = await authService.authenticateUser(req.query.state, user);
-            res.redirect(`${process.env.CLIENT_APP_URl}/success?resp=${resp}`); 
-        } catch (error) {
-            res.redirect(`${process.env.CLIENT_APP_URl}/failure?err=${error.message}`); 
-        }
-
-    })(req, res, next);
+  passport.authenticate(
+    "google",
+    { session: false, failureRedirect: "/" },
+    async (err, user, info) => {
+      if (err || !user) {
+        return res
+          .status(401)
+          .json({ error: "Authentication failed. Please try again." });
+      }
+      try {
+        const resp = await authService.authenticateUser(req.query.state, user);
+        const respString = encodeURIComponent(JSON.stringify(resp));
+        res.redirect(
+          `${process.env.CLIENT_APP_URl}/success?resp=${respString}`
+        );
+      } catch (error) {
+        res.redirect(
+          `${process.env.CLIENT_APP_URl}/failure?err=${error.message}`
+        );
+      }
+    }
+  )(req, res, next);
 };
-
